289064b8f8c1e1fcf102882e8ca681247d8defa2
Jonathan Linowes <jonathan@parkerhill.com>
2016-06-12 00:56:45 -0400

    upgrade to GVR SDK 0.8.0

--- renderbox/src/main/java/com/cardbookvr/renderbox/RenderBox.java
+++ renderbox/src/main/java/com/cardbookvr/renderbox/RenderBox.java
@@ -10,10 +10,10 @@ import com.cardbookvr.renderbox.components.Light;
 import com.cardbookvr.renderbox.components.RenderObject;
 import com.cardbookvr.renderbox.materials.VertexColorLightingMaterial;
 import com.cardbookvr.renderbox.materials.VertexColorMaterial;
-import com.google.vrtoolkit.cardboard.CardboardView;
-import com.google.vrtoolkit.cardboard.Eye;
-import com.google.vrtoolkit.cardboard.HeadTransform;
-import com.google.vrtoolkit.cardboard.Viewport;
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +23,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 /**
  * Created by Schoen and Jonathan on 4/16/2016.
  */
-public class RenderBox implements CardboardView.StereoRenderer {
+public class RenderBox implements GvrView.StereoRenderer {
     private static final String TAG = "RenderBox";
 
     public static RenderBox instance;
